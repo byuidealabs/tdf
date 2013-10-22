b9e787249a8729ae6ecb6790a5306d186cb844cb
Nathan Woodbury <nathanswoodbury@gmail.com>
2013-10-21 20:50:17 -0600

    Upgraded test system for metrics.

--- public/js/controllers/testmetrics.js
+++ public/js/controllers/testmetrics.js
@@ -1,12 +1,21 @@
 angular.module('tdf').controller('TestMetricsController',
     ['$scope', 'Metrics',
     function ($scope, Metrics) {
-        $scope.test1 = {
-            metric : 'max',
-            values : [10, 7, 12, 8],
-            expected_score : 12,
-            expected_moving : [10, 10, 12, 12]
-        };
+
+        $scope.tests = [
+            {
+                metric : 'max',
+                values : [10, 7, 12, 8],
+                expected_score : 12,
+                expected_moving : [10, 10, 12, 12]
+            },
+            {
+                metric : 'last',
+                values : [10, 7, 12, 8],
+                expected_score : 8,
+                expected_moving : [10, 7, 12, 8]
+            }
+        ];
 
         $scope.testScore = function(values, metric) {
             return Metrics.score(values, metric);
