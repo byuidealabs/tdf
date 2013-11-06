aafb8f38c05d017082ebe0654a6421dd5e5a9e5a
Nathan Woodbury <nathanswoodbury@gmail.com>
2013-11-06 11:48:52 -0700

    Some header navigation improvements.

--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -1,15 +1,25 @@
-angular.module('tdf.system').controller('HeaderController', 
-    ['$scope', 'Global', 
+angular.module('tdf.system').controller('HeaderController',
+    ['$scope', 'Global',
     function ($scope, Global) {
         $scope.global = Global;
 
         $scope.menu = [
+            {
+                'title': 'Profile',
+                'link': 'users/profile'
+            },
             {
                 'title': 'Leagues',
                 'link': 'leagues/'
             },
             {
                 'title': 'Users',
-                'link': 'users/'}];
-        
+                'link': 'users/'
+            },
+            {
+                'title': 'Agents',
+                'link': 'agents/'
+            }
+        ];
+
     }]);
