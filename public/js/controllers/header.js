e06ae4a107259ddd66db5b4379471c839c028646
Nathan Woodbury <nathanswoodbury@gmail.com>
2013-11-07 14:22:02 -0700

    Improved header nav--private/public parts.

--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -3,19 +3,36 @@ angular.module('tdf.system').controller('HeaderController',
     function ($scope, Global) {
         $scope.global = Global;
 
-        $scope.menu = [
-            {
-                'title': 'My Agents',
-                'link': 'users/' + $scope.global.user._id
-            },
-            {
-                'title': 'Leagues',
-                'link': 'leagues/'
-            },
-            {
-                'title': 'All Users',
-                'link': 'users/'
+        $scope.setMenu = function() {
+            if (!$scope.global.user) {
+                $scope.menu = $scope.getPublicMenu();
             }
-        ];
+            else {
+                $scope.menu = $scope.getPrivateMenu();
+            }
+        };
 
+        $scope.getPublicMenu = function() {
+            return [
+                {
+                    'title': 'Leagues',
+                    'link': 'leagues/'
+                },
+                {
+                    'title': 'All Users',
+                    'link': 'users'
+                }
+            ];
+        };
+
+        $scope.getPrivateMenu = function() {
+            var public_menu = $scope.getPublicMenu();
+            var private_menu = [
+                {
+                    'title': 'My Agents',
+                    'link': 'users/' + $scope.global.user._id
+                }
+            ];
+            return public_menu.concat(private_menu);
+        };
     }]);
